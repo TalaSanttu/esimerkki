f520901f0fe4f2455bf7c92a37e965bcced0fbe3
juhay-git <78492068+juhay-git@users.noreply.github.com>
2023-09-14 12:59:40 +0300

    Konfliktit resolvattu

--- koodiesimerkki.py
+++ koodiesimerkki.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 asd
 asdiddiidiii
-=======
+
 asdasd
 asd
 
-<<<<<<< HEAD
->>>>>>> 62c4faba421e124ecea4a83f737f039c6fc1afd6
-=======
 tuplaasd
->>>>>>> a9183eff91527e8efa00ee87481b816a5bdcf1de
